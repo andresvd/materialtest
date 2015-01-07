d913184e2f8a9d6bf0c1ccb8f5dc6466fcd12aa2
Andre van Drunen <andre.drunen@openbossa.com>
2015-01-07 14:04:54 -0300

    Navigation Drawer funcionando (aparecendo) com ícones sem clicar em nada. Parei no vídeo #16

--- app/src/main/java/materialtest/com/example/andr/materialtest/DrawerAdapter.java
+++ app/src/main/java/materialtest/com/example/andr/materialtest/DrawerAdapter.java
@@ -1,32 +1,57 @@
 package materialtest.com.example.andr.materialtest;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by André on 06/01/2015.
  */
 public class DrawerAdapter extends RecyclerView.Adapter<DrawerAdapter.MyViewHolder> {
+
+    private LayoutInflater inflater;
+    List<Information> data = Collections.emptyList();
+    private Context context;
+    public DrawerAdapter(Context context, List<Information> data) {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+        this.data = data;
+    }
+
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.custom_row, parent,false);
+        MyViewHolder holder = new MyViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-
+        Information current = data.get(position);
+        holder.title.setText(current.title);
+        holder.icon.setImageResource(current.iconId);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return data.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder{
-
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        ImageView icon;
         public MyViewHolder(View itemView) {
             super(itemView);
+            title = (TextView) itemView.findViewById(R.id.listText);
+            icon = (ImageView) itemView.findViewById(R.id.listIcon);
         }
     }
 
--- app/src/main/java/materialtest/com/example/andr/materialtest/NavigationDrawerFragment.java
+++ app/src/main/java/materialtest/com/example/andr/materialtest/NavigationDrawerFragment.java
@@ -7,11 +7,15 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +30,7 @@ public class NavigationDrawerFragment extends Fragment {
     private boolean mUserLearnedDrawer;
     private boolean mFromSavedInstanceState;
     private View containerView;
+    private DrawerAdapter adapter;
 
     public NavigationDrawerFragment() {
         // Required empty public constructor
@@ -46,7 +51,24 @@ public class NavigationDrawerFragment extends Fragment {
         // Inflate the layout for this fragment
         View layout = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
         recyclerView= (RecyclerView) layout.findViewById(R.id.drawerList);
+        adapter = new DrawerAdapter(getActivity(), getData());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         return layout;
+    }
+
+    public static List<Information> getData(){
+        List<Information> data = new ArrayList<>();
+        int[] icons = {R.drawable.ic_magnify_grey600_24dp, R.drawable.ic_history_grey600_24dp, R.drawable.ic_account_grey600_24dp, R.drawable.ic_trophy_grey600_24dp, R.drawable.ic_format_list_numbers_grey600_24dp, R.drawable.ic_cog_grey600_24dp};
+        String[] titles = {"Nova Busca", "Buscas Anteriores", "Perfil", "Troféus", "Ranking", "Preferências"};
+        for (int i = 0; i < titles.length && i < icons.length; i++){
+            Information current = new Information();
+            current.iconId = icons[i];
+            current.title = titles[i];
+            data.add(current);
+        }
+
+        return data;
     }
 
 
